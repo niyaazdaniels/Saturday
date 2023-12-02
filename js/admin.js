f7f65953e9ca1afe5e5eeb7ae84deac483674412
Niyaaz Daniels <niyaazdaniels1@gmail.com>
2023-12-02 14:15:10 +0200

    commit

---
+++ js/admin.js
@@ -0,0 +1,126 @@
+// multiple objects required for products
+// make use of constrcutor function to create 
+
+// name description price and image URL
+// additional information can be added to products
+
+
+// this is where all products will be stored
+
+let items = [];
+
+// object created manually
+// dont do this way - must use a constructor function to make the function
+let object1 = {
+    id: 1,
+    name: "Air",
+    description: "Legend",
+    price: 1500 ,
+    url:"https://static.nike.com/a/images/t_default/9b26aa8f-0173-409b-b30a-7ce2d88573a4/custom-nike-dunk-low-by-you.png"
+}
+// in order to create objects we will need a constructor function
+
+// constructor function that will create objects
+
+// items will be pushed into the array
+// function to create objects
+function ConstructorBro (id, name, description, price, url) {
+    this.id = id,
+    this.name = name,
+    this.description = description,
+    this.price = price,
+    this.url = url
+}
+// second item created using constructor 
+let item2 = new ConstructorBro(2, "Nike"," The OG", 800, "https://static.nike.com/a/images/t_default/q3tx0zbjfdhuayuuqtj4/ebernon-low-shoe-04dgFq.png")
+// to push item2 into array called items 
+items.push(object1,item2)
+// saves items into the browser from the array into local storage
+
+localStorage.setItem("items",JSON.stringify(items))
+// this items is the array, once we store the data, we get the data and that data is stored into the array, so it will be received directly from the browser
+// set items into browser, get items from browser and stores into array
+// converts saved data in the local storage to an object
+items = JSON.parse(localStorage.getItem("items"))
+
+// linking
+// query selector to display information
+
+let table = document.querySelector('table')
+// 2 parameters items and index because the array is called items
+// map takes 2 parameters loops through objects
+// index will return the index of the object
+// map creates a new array
+
+// console.log(items);
+// console.log(index);
+// creating the tag we want to insert
+// because information is going inside a table
+function niyaaz(){
+let products = items.map(function(item, index){
+    return `
+
+    <tr>
+    <th>ID</th>
+    <th>Name</th>
+    <th>Description</th>
+    <th>Price</th>
+    <th>Image</th>
+    </tr>
+        <tr>
+            <td> ${item.id}: </td>
+            <td> ${item.name} </td>
+            <td> ${item.description} </td>
+            <td> R${item.price} </td>
+            <td> <img src ="${item.url}"</td>
+            <td><button>Edit</button></td>
+            <td><button class ="delete" value ="${index}">Delete</button></td>
+        </tr> `
+});
+// add to cart button will have a certain index
+// push that item to new array and stores in local storage and previewed in checkout 
+
+// create a function that when the button is clicked it does something to item
+//function to remove data
+function remove(position){
+    items.splice(position,1)
+    favourite()
+    niyaaz()
+}
+let dltBtn = document.querySelector('.delete')
+// to the parent tag where everything is written to which is table
+// adding event listerner to table parent tag
+// can only appear in certain tags
+table.addEventListener('click', function(){
+    // will affect all the children tags of the parent tag
+    if (event.target.classList.contains('delete')){
+        remove(event.target.value)
+        // alert(event.target.value)
+        // returns the index in an alert
+    }
+})
+//writing products to the table in HTML page
+//  instead of using this.id we can make use to  index+1 to add
+// we use .join() to remove the comment
+table.innerHTML = products.join('')
+}niyaaz()
+//nested function
+//nested
+function favourite() {
+    localStorage.setItem("items",JSON.stringify(items))
+    items = JSON.parse(localStorage.getItem("items"))
+}
+// we need a spinner
+// make use of bootstrap spinner
+// use js to hide it
+// if data doesn't run the spinner must display
+
+// to style table
+table.style.backgroundColor = 'cornflowerblue'
+table.style.color = 'white'
+table.style.textAlign = 'center'
+
+// conditional statement 
+// else = table.style.display = 'none' - hides spinner when data is shown
+
+// if we call a function as a parameter within another function is called a callback function
---
+++ js/products.js
@@ -0,0 +1,40 @@
+//selecting the main tag because we want to display data in it
+
+// add to cart values to be added to new array purchased
+let purchased = []
+let main = document.querySelector('main')
+let items = JSON.parse(localStorage.getItem('items'))
+
+main.innerHTML = items.map(function(item, index){
+    return `
+    <div> 
+    <h2>${item.name}</h2>
+    <h5>${item.description}</h5>
+    <h5>${item.price}</>
+    <h5><img src = ${item.url}></h5>
+    <button value= '${index}' data-add >Add to Cart </button>
+        </div>
+        `
+}).join('')
+
+//can use reduce to add all the amount prices together
+
+function add(index){
+    purchased.push(items[index])
+    localStorage.setItem("purchased",JSON.stringify(purchased))
+}
+main.addEventListener('click', function(){  
+    if (event.target.hasAttribute('data-add')){
+        // alert ('Button Clicked')
+        add(event.target.value)
+    }
+})
+
+
+// search bar
+
+let a = items.filter(item => {
+    return item.name == 'Air' 
+})
+
+// adding extra code for github
